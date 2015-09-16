a09b173ce2a14c4e838248ee864740399f6b020b
Marc Ramser <marc.ramser@hotmail.com>
2015-09-16 20:11:03 +0200

    LumX installiert

---
+++ bower_components/lumx/core/js/lumx.js
@@ -0,0 +1,25 @@
+/* global angular */
+
+angular.module('lumx.utils', [
+    'lumx.utils.transclude',
+    'lumx.utils.transclude-replace',
+    'lumx.utils.event-scheduler'
+]);
+
+angular.module('lumx', [
+    'lumx.utils',
+    'lumx.ripple',
+    'lumx.notification',
+    'lumx.dropdown',
+    'lumx.text-field',
+    'lumx.dialog',
+    'lumx.select',
+    'lumx.scrollbar',
+    'lumx.thumbnail',
+    'lumx.tabs',
+    'lumx.tooltip',
+    'lumx.file-input',
+    'lumx.progress',
+    'lumx.search-filter',
+    'lumx.date-picker'
+]);
