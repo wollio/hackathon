15f3c4c39361e5fb4a5fe04d8d4ac841cc362e61
Marc Ramser <marc.ramser@hotmail.com>
2015-09-25 19:44:35 +0200

    no message

--- www/app/controllers/history.controller.js
+++ www/app/controllers/history.controller.js
@@ -1,3 +1,9 @@
 angular.module('feature.history')
-    .controller('HistorysCtrl', function($scope, $rootScope, $location, $state){
+    .controller('HistorysCtrl', function($scope, $rootScope, $location, $state, $http){
+        $http.get('http://hackathon-secanis.rhcloud.com/index.php/api/history').
+            then(function (resp) {
+                $scope.history = resp.data;
+            }, function (response) {
+                $rootScope.$broadcast('error');
+            });
     });
