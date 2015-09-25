72a45d0fe8ecd1fc4609ffb6cf0f34f4c2cac08c
David Wollschlegel <david@wollschlegel.com>
2015-09-26 01:48:40 +0200

    asd
    asd

--- www/app/controllers/reward.controller.js
+++ www/app/controllers/reward.controller.js
@@ -1,46 +1,47 @@
 angular.module('feature.reward')
-    .controller('RewardCtrl', function($scope, $rootScope, $location, $state, $http, $stateParams, $mdDialog){
-        $rootScope.$broadcast('changeMenuVisibility', false);
-        var id = $stateParams.id;
-        $scope.currentQuestLevel = $stateParams.level;
-        $http.get('https://pfhackathon.secanis.ch/api/reward/' + id).
-            then(function (resp) {
-                $scope.rewards = resp.data;
+  .controller('RewardCtrl', function($scope, $rootScope, $location, $state, $http, $stateParams, $mdDialog) {
+    $rootScope.$broadcast('changeMenuVisibility', false);
+    var id = $stateParams.id;
+    $scope.currentQuestLevel = $stateParams.level;
+    $http.get('https://pfhackathon.secanis.ch/api/reward/' + id).
+    then(function(resp) {
+      $scope.rewards = resp.data;
 
-            }, function (response) {
-                $rootScope.$broadcast('error');
-            });
-
-        $scope.redeem = function(reward) {
-
-
-
-          var confirm = $mdDialog.confirm()
-            .title('Reward anfordern')
-            .content('Möchten Sie den Reward anfordern?')
-            .ariaLabel('bla')
-            .ok('Ja, gerne')
-            .cancel('Nein danke');
-          $mdDialog.show(confirm).then(function() {
-
-              $http.put('https://pfhackathon.secanis.ch/api/reward/' + reward.rewardID, {}).
-                  then(function(response) {
-                    reward.inUse = 1;
-                    alert = $mdDialog.alert({
-                        title: 'Vielen Dank',
-                        content: 'Sie werden in den nächsten Tagen von uns schriftlich benachrichtig.',
-                        ok: 'Schliessen'
-                    });
-                    $mdDialog
-                        .show( alert )
-                        .finally(function() {
-                            alert = undefined;
-                        });
-                  }, function(response) {
-                      $rootScope.$broadcast('error');
-                  });
-          }, function() {
-
-          });
-        }
+    }, function(response) {
+      $rootScope.$broadcast('error');
     });
+
+    $scope.redeem = function(reward, currentlevel) {
+
+      if (reward.inUse == 0 && currentlevel >= reward.level) {
+
+      var confirm = $mdDialog.confirm()
+        .title('Reward anfordern')
+        .content('Möchten Sie den Reward anfordern?')
+        .ariaLabel('bla')
+        .ok('Ja, gerne')
+        .cancel('Nein danke');
+      $mdDialog.show(confirm).then(function() {
+
+        $http.get('https://pfhackathon.secanis.ch/api/updatereward/' + reward.rewardID)
+          .then(function(response) {
+            reward.inUse = 1;
+            alert = $mdDialog.alert({
+              title: 'Vielen Dank',
+              content: 'Sie werden in den nächsten Tagen von uns schriftlich benachrichtig.',
+              ok: 'Schliessen'
+            });
+            $mdDialog
+              .show(alert)
+              .finally(function() {
+                alert = undefined;
+              });
+          }, function(response) {
+            $rootScope.$broadcast('error');
+          });
+      }, function() {
+
+      });
+    }
+  }
+  });
