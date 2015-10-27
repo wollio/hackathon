49503091bd958f3415ea5bd1b9b4746fffeb421a
Matthias Baldi <matthias.baldi@post.ch>
2015-10-27 08:47:57 +0100

    clean up platforms

--- platforms/wp8/www/app/controllers/settings.controller.js
+++
@@ -1,4 +0,0 @@
-angular.module('feature.settings')
-    .controller('SettingsCtrl', function($scope, $rootScope, $location, $state){
-        $rootScope.$broadcast('changeMenuVisibility', false);
-    });
